60b498933eebd22efb72ce34b504edda7b9f5a49
권 태헌 <pcloud@scglab.com>
2023-01-04 12:23:54 +0900

    feat: LB 해결
    잊고 있었다. 유레카 클라이언트의 이름은 eureka.instance.appname에 설정하는 것이 아닌, spring.application.name으로 지정하는 것.

--- components/module-member/src/main/java/devgraft/member/api/MemberApi.java
+++ components/module-member/src/main/java/devgraft/member/api/MemberApi.java
@@ -5,6 +5,7 @@ import devgraft.member.app.SignUpUseCase.SignUpRequest;
 import devgraft.member.app.SignUpUseCase.SignUpResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberApi {
     private final SignUpUseCase signUpUseCase;
+
+    @GetMapping
+    public String home() {
+        return "MY MEMBER-MICRO-SERVICE";
+    }
 
     @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping("sign-up")
